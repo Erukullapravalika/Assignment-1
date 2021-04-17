9886d486205aa029546c96de71bc34f246b6c8b0
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 12:31:53 +0530

    First assignment

---
+++ fibbonaci/src/Fib.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Fib {
+	public static void main(String args[]) {
+	int n1=0,n2=1,n3,sum=10;
+	for(int i=2;i<=sum;i++) {
+		n3=n1+n2;
+		System.out.println(n3);
+		n1=n2;
+		n2=n3;
+		
+
+				
+				}
+			
+		
+
+	}
+
+}
