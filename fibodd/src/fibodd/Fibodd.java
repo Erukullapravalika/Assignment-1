a457a1ef157e2df10e5f7b8d9db4efd97c5a9438
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 12:51:51 +0530

    Fibodd

---
+++ fibodd/src/fibodd/Fibodd.java
@@ -0,0 +1,28 @@
+package fibodd;
+
+public class Fibodd {
+	public static void main(String[] args) {
+		int n=0,n1=0,n2=1;
+        	 for(int i=0;i<20;i++) {
+        		 n1=n2;
+        		 n2=n;
+        		 n=n1+n2;
+        		 if(n1 % 2!=0 && n1<=100)
+        		
+        	 System.out.println(n1 + " ");
+        	 }
+        			
+
+	
+        		
+		    	
+		// TODO Auto-generated method stub
+
+	    
+	}
+   
+	}
+
+
+
+
