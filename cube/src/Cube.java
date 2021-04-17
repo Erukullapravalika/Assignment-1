1f60c555e61a21defda2629e30cf15b7e6460cbd
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 12:25:02 +0530

    First assignment

---
+++ cube/src/Cube.java
@@ -0,0 +1,17 @@
+
+public class Cube {
+
+	public static void main(String[] args) {
+		int number=10,cube;
+		for(int i=1;i<=number;i++) 
+		{
+			cube=i*i*i;
+		
+		System.out.println("The cubes of numbers is:"+cube);
+		}
+		
+	
+
+	}
+
+}
