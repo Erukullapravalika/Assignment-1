acbcb3a8e92dd230d3b2484db4d3d2ce8b290fd9
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 12:09:30 +0530

    First assignment

---
+++ Fahrenheit/src/Fahrenheit.java
@@ -0,0 +1,14 @@
+
+public class Fahrenheit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double f=96,c;
+		c=(f-32)*5/9;
+		System.out.println("celsius temperature is:"+c);
+		
+		
+
+	}
+
+}
