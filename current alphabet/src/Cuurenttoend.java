9c0f70d0a8148ad6086a5dad42bef956f84db18c
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 12:11:18 +0530

    First assignment

---
+++ current alphabet/src/Cuurenttoend.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Cuurenttoend {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter your current alphabet:");
+		char ch=scanner.next().charAt(0);
+			for(char i=ch;i<='z';i++)
+			{
+				System.out.println(i+ "");
+			}
+		
+		}
+	}
+
+		
+				
+	
+
+
