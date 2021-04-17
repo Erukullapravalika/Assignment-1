b2b771c02897753a5133698373773154ae54329b
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 12:13:43 +0530

    First assignment

---
+++ factorial/src/Factorial.java
@@ -0,0 +1,16 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int i,number=5,fact=1;
+		for( i=1;i<=number;i++) {
+			
+			fact=fact*i;
+			
+		}
+		System.out.println("The factorial of a number is:"+fact);
+		// TODO Auto-generated method stub
+
+	}
+
+}
