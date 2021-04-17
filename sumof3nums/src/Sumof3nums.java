a1daf392c168a1fd61e4c50ca157ae38b16f0a26
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 12:20:37 +0530

    First assignment

---
+++ sumof3nums/src/Sumof3nums.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sumof3nums {
+
+	public static void main(String[] args) {
+		int num1,num2,num3,sum=0;
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter first number:");
+		num1=scanner.nextInt();
+		System.out.println("enter second number:");
+		num2=scanner.nextInt();
+		System.out.println("Enter third number:");
+		num3=scanner.nextInt();
+		sum=num1+num2+num3;
+		System.out.println("the sum of these numbers is:" +sum);
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
